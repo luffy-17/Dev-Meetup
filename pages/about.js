77dc0ef28509ac65741ad99d1cd862548d02bee8
Laxmikant <laxmikantn07r@gmail.com>
2022-01-26 15:19:58 +0500

    page routing for stater

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+//jshint esversion:6
+const About = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This is an app for developers to meet and talk about nerdy stuff.</p>
+            <p>version: 1.0.0</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ pages/events/index.js
@@ -0,0 +1,10 @@
+//jshint esversion:6
+const EventsPage = () => {
+    return (
+        <div>
+            <h1>My Events</h1>
+        </div>
+    );
+}
+
+export default EventsPage;
